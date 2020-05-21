2383b292314d8e7a0e61adc693dd390fc5cfc014
Toni  Diep <adiep123@gmail.com>
2020-05-21 17:59:10 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,15 +7,14 @@ class Person
 
   def get_married(person)
     self.partner = person
-    # if 
-      # person.class != Person
-      # raise PartnerError
-    # else
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
-    # end
+    end
   end
-  # class PartnerError < StandardError
-  # end
+  class PartnerError < StandardError
+  end
   
 end
 
