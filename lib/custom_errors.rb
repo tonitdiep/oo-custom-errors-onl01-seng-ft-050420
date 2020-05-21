67de47e663e8c2616f629a8ef8860d5071e12f3a
Toni  Diep <adiep123@gmail.com>
2020-05-21 17:38:08 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
+    end
   end
-
+  class PartnerError < StandardError
+  end
+  
 end
 
 beyonce = Person.new("Beyonce")
